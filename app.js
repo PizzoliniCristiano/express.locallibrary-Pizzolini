68018c5fad0a16817757a62cc358c840e834369c
PizzoliniCristiano <127383453+PizzoliniCristiano@users.noreply.github.com>
2025-05-16 08:52:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 const dev_db_url =
-  "mongodb+srv://cristianopizzolini:Kripiz2024@cluster0.uzbmv5z.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
+
+"mongodb+srv://cooluser:coolpassword@cluster0.u0jkl.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 
